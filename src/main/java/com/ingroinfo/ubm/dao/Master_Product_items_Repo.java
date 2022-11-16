d2930238b2bf6de6b3e04e21f4f85a798efce9a2
Panneer <response@ingroinfo.com>
2022-11-16 15:12:26 +0530

    Updates

--- src/main/java/com/ingroinfo/ubm/dao/Master_Product_items_Repo.java
+++
@@ -1,8 +0,0 @@
-package com.ingroinfo.ubm.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.ingroinfo.ubm.entity.Master_Product;
-
-public interface Master_Product_items_Repo extends JpaRepository<Master_Product, Integer> {
-
-}
--- src/main/resources/static/js/number-match.js
+++ src/main/resources/static/js/number-match.js
@@ -1,9 +1,29 @@
 var check = function() {
-         if (document.getElementById('mobile').value ==
-         document.getElementById('alternateMobile').value) {
-         document.getElementById('message').style.color = "red";
-         document.getElementById('message').innerHTML = "Alternate mobile no shouldn't same as mobile no field";
-         } else {
-         document.getElementById('message').innerHTML = "";
-         }
-         }
+	if (document.getElementById('mobile').value ==
+		document.getElementById('alternateMobile').value) {
+		document.getElementById('message').style.color = "red";
+		document.getElementById('message').innerHTML = "Alternate mobile no shouldn't same as mobile no field";
+	} else {
+		document.getElementById('message').innerHTML = "";
+	}
+}
+
+var checkMobile = function() {
+	if (document.getElementById('emobileCopy').value ==
+		document.getElementById('ealternateMobileCopy').value) {
+		document.getElementById('message').style.color = "red";
+		document.getElementById('message').innerHTML = "Alternate mobile no shouldn't same as mobile no field";
+	} else {
+		document.getElementById('message').innerHTML = "";
+	}
+}
+
+var checkMobileOwner = function() {
+	if (document.getElementById('mobileNoCopy').value ==
+		document.getElementById('alternateMobileCopy').value) {
+		document.getElementById('message').style.color = "red";
+		document.getElementById('message').innerHTML = "Alternate mobile no shouldn't same as mobile no field";
+	} else {
+		document.getElementById('message').innerHTML = "";
+	}
+}
