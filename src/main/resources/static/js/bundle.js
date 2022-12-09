3e4f34d7312ae8476e0924f7e5fb61e71d3d88e4
Panneer <Panneer867@live.com>
2022-12-09 18:08:11 +0530

    update

--- src/main/resources/static/js/bundle.js
+++ src/main/resources/static/js/bundle.js
@@ -1,39 +1,25 @@
 
 $(".add-row").click(function() {
-	// $(".demorow").remove();
+
 	const $this = $("table tbody");
 	const lastIndex = Number($this.find("td.sl_no:last").text());
 	const incremented = lastIndex + 1;
 	const rowname = $("#books").val();
-	const quantity = $("#qty").val();
 	const itemPrice = $("#itemPrice").val();
 
-
+	$('.bookName').text(rowname);
+	$('.price').val(itemPrice);
 
 	if (rowname.length > 0) {
 		$('.msg-show').hide();
-		
-		let markup = "<tr><td class='sl_no bg-secondary py-1 text-dark'>" + incremented + "</td><td class='bg-info py-1'>" + rowname;
-		markup += '</td><td class="bg-success py-1 pick-quantity">' + quantity
-		markup += '</td><td class="bg-warning py-1"><i class="fa fa-inr pr-1" aria-hidden="true" style="font-size:13px"></i>' + itemPrice
-		markup += '</td><td class="bg-danger py-1 text-center">'
-		markup += '<button class="p-0 btn btn-sm bg-transparent deleterow" type="submit"><i class="glyphicon glyphicon-trash text-white"></i>'
-		markup += '</button></td></tr>';
+		$('.grand').show();
+
+		let markup = "<tr><td class='sl_no'><label class='mb-0'>" + incremented + "</label></td></tr>";
+
 		$("table tbody").append(markup);
 	}
-});
-
-
-$("table tbody").on("click", ".deleterow", function() {
-
-	$(this).parent().parent().remove();
-	$("tr .sl_no").each(function(i) {
-		// Table tr seriaal  number update.
-		$(this).text(i + 1);
-	});
 
 });
-
 
 $(function() {
 
@@ -72,13 +58,12 @@ $(function() {
 		});
 		$('.subtotal').val((subtotalVal).toFixed(2));
 
-		$(".vat").val(((subtotalVal / 100) * 20).toFixed(2));
+		$(".vat").val(((subtotalVal / 100) * 18).toFixed(2));
 
-		var vatVal = ((subtotalVal / 100) * 20).toFixed(2);
+		var vatVal = ((subtotalVal / 100) * 18).toFixed(2);
 		var total = parseFloat(subtotalVal) + parseFloat(vatVal);
 		$(".total").val((total).toFixed(2));
 	}
-
 
 	$(".glyphicon-trash").click(function() {
 		$(this).parent().parent().remove();
@@ -87,3 +72,6 @@ $(function() {
 
 });
 
+$(document).ready(function() {
+	$('.grand').hide();
+});
